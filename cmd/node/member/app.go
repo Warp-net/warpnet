fd356517075b0110588beb2e534ea27b1030485b
vadim <filinvadim@protonmail.com>
2025-07-31 15:08:04 +0200

    WIP

--- cmd/node/member/app.go
+++ cmd/node/member/app.go
@@ -285,7 +285,7 @@ func newErrorResp(msg string) stdjson.RawMessage {
 func (a *App) close(_ context.Context) {
 	defer func() { recover() }()
 
-	log.Infoln("closing...")
+	log.Infoln("app: closing...")
 
 	a.db.Close()
 	a.node.Stop()
