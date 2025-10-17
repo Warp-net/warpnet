867a158847fbb2fa4f4a3d74e7710a43f46e33c6
vadim <filinvadim@protonmail.com>
2025-10-17 15:10:31 +0200

    Update deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-echo "Run deploy script"
+#sudo ufw disable
+#sudo systemctl restart systemd-networkd
+#sudo iptables -I DOCKER-USER -j ACCEPT
+#sudo iptables -A INPUT -p tcp --match multiport --dports 4001:4003 -j ACCEPT
+#sudo iptables -A FORWARD -i docker0 -o docker0 -j ACCEPT
+#sudo iptables -A FORWARD -i br-6383b19e4979 -o br-6383b19e4979 -j ACCEPT
+#sudo iptables -P FORWARD ACCEPT
+#sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 #echo "nameserver 8.8.8.8" > /etc/resolv.conf
 #echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+
+echo "Run deploy script"
+
 echo "GITHUB_TOKEN: ${GITHUB_TOKEN:0:4}... (truncated for security)"
 
 if [ -z "$GITHUB_TOKEN" ]; then
@@ -13,15 +24,7 @@ fi
 echo $GITHUB_TOKEN | sudo docker login ghcr.io -u filinvadim --password-stdin
 docker pull ghcr.io/warp-net/warpnet-bootstrap:latest
 docker pull ghcr.io/warp-net/warpnet-moderator:latest
-#sudo ufw disable
-#sudo systemctl restart systemd-networkd
-#sudo iptables -I DOCKER-USER -j ACCEPT
-#sudo iptables -A INPUT -p tcp --match multiport --dports 4001:4003 -j ACCEPT
-#sudo iptables -A FORWARD -i docker0 -o docker0 -j ACCEPT
-#sudo iptables -A FORWARD -i br-6383b19e4979 -o br-6383b19e4979 -j ACCEPT
-#sudo iptables -P FORWARD ACCEPT
-#sudo iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-#sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
 touch /tmp/snapshot || true
 export HOSTNAME=''
 
