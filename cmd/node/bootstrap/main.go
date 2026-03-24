8f25ec6a3e9256309b2172b1f08c9d6700cde5e8
vadim <filinvadim@protonmail.com>
2026-03-24 12:20:06 +0100

    Chores

--- cmd/node/bootstrap/main.go
+++ cmd/node/bootstrap/main.go
@@ -99,6 +99,9 @@ func main() {
 	}
 
 	if config.Config().Socks5.IsEnabled {
+		if config.Config().Node.IsPskPrinted {
+			log.Infof("CURRENT PSK: %s", psk.String())
+		}
 		port := config.Config().Socks5.Port
 		srv := socks5.NewServer(ctx, port, psk.String())
 		if err := srv.Start(n); err != nil {
