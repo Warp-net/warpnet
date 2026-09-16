962add92f80f17b4dde91975d3f31355ba2ddd19
Vadim Filin <github.com.mecdy@passmail.net>
2026-09-16 18:15:54 +0200

    Update gossip.go

--- core/pubsub/gossip.go
+++ core/pubsub/gossip.go
@@ -246,7 +246,7 @@ func (g *Gossip) runGossip() (err error) {
 	}
 	g.isRunning.Store(true)
 
-	go g.runPeerInfoPublishing(time.Minute * 5)
+	go g.runPeerInfoPublishing(time.Minute * 30)
 	log.Infoln("gossip: started")
 
 	return
